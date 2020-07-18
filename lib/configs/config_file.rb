1e382d5aa208de760f401805d4bbf02a67cc22b5
Paul Sonkoly <sonkoly.pal@gmail.com>
2020-07-18 10:04:06 +0100

    config file

--- lib/configs/config_file.rb
+++ lib/configs/config_file.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'erb'
+
 module Configs
   # Receives settings in form of a binding, and runs an erb template on the
   # binding. It writes the results into a predefined location.
@@ -8,10 +10,24 @@ module Configs
     # @param template [String] template file location
     # @param location [String] where to write resulting config
     # @param binding [Binding] bindings for erb template
-    def initialize(template, location)
+    def initialize(template, location, binding)
       @template = template
       @location = location
       @binding = binding
+    end
+
+    def call
+      File.write(@location, content)
+    end
+
+    private
+
+    def content
+      erb_template.result(@binding)
+    end
+
+    def erb_template
+      ERB.new(File.read(@template))
     end
   end
 end
--- spec/configs/config_file_spec.rb
+++ spec/configs/config_file_spec.rb
@@ -3,9 +3,57 @@
 require 'spec_helper'
 
 require 'configs/config_file'
+require 'tmpdir'
 
 RSpec.describe Configs::ConfigFile do
-  it 'fails' do
-    expect(true).to be_falsey
+  let(:fake_binding) do
+    binding = nil
+    Object.new.instance_eval do
+      @fake_setting = 'value'
+      binding = self.binding
+    end
+    binding
+  end
+
+  let(:tmpdir) { Dir.mktmpdir }
+
+  let(:fake_template) do
+    File.join(tmpdir, 'fake_config.erb')
+  end
+
+  let(:fake_output_location) do
+    File.join(tmpdir, 'fake_config')
+  end
+
+  before :each do
+    File.write(fake_template, <<~END_OF_TEMPLATE)
+      Fake config file
+      ================
+      # containing only a single setting whose value is 'value'
+      fake_setting = <%= @fake_setting %>
+    END_OF_TEMPLATE
+  end
+
+  after :each do
+    FileUtils.rm_f(fake_template)
+  end
+
+  describe '#call' do
+    subject { described_class.new(fake_template, fake_output_location, fake_binding) }
+
+    it 'writes the expected config file' do
+      expect { subject.call }.to change { File.exist?(fake_output_location) }.from(false).to(true)
+    end
+
+    it 'writes a config file with the expected content' do
+      subject.call
+      content = File.read(fake_output_location)
+      expect(content).to eq <<~EXPECTED_OUTPUT_END
+        Fake config file
+        ================
+        # containing only a single setting whose value is 'value'
+        fake_setting = value
+      EXPECTED_OUTPUT_END
+    end
   end
 end
